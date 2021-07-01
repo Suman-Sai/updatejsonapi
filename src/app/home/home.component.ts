aa81d73f7645e7aa032978e3d23812c3b47eb009
Suman-Sai <sumansai454@gmail.com>
2021-07-01 21:38:43 +0530

    initial commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $("#photo").click(function(){
+      $("#photo").css({"transform":"translate(-450px) translateY(100px) scale(3)",
+    "border-radius":"100px",
+  "border":"none",
+"opacity":"1"});
+  $("main").css("opacity","0.5")
+    });
+    $("#photo").dblclick(function(){
+      $("#photo").css({"transform":"translate(0px) translateY(0px) scale(1)",
+    "border-radius":"100px",
+  "border":"2px solid black",
+"opacity":"1"});
+  $("main").css("opacity","1")
+    })
   }
 
 }
